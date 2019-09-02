8379abafa80e7feb9cce84c8bc3d864b06f227be
guyaoyao <851511476@qq.com>
2019-09-02 16:28:42 +0800

    no message
    Change-Id: I45aca5c33044bf2b6ea5cf572c977fb85b4d0777

---
+++ dist/precache-manifest.a035da6635b11e103cd7bf88018b05c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  },
+  {
+    "revision": "da5beaf91079a6ed792b",
+    "url": "/dist/js/chunk-vendors.9080b304.js"
+  },
+  {
+    "revision": "218554de72c0de797843",
+    "url": "/dist/js/app.2cd7be86.js"
+  },
+  {
+    "revision": "9b2b6ada5d051209429b",
+    "url": "/dist/js/about.8fd4cd74.js"
+  },
+  {
+    "revision": "5aa4236d49347d8cd0ea96008254aa74",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "218554de72c0de797843",
+    "url": "/dist/css/app.38f827ec.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71d24c475d9614626664bd8d95e85e57.js"
+  "/dist/precache-manifest.a035da6635b11e103cd7bf88018b05c0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "myself"});
