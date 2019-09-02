b1764db9f570658b761bab1fc8cca556becf5e7e
guyaoyao <851511476@qq.com>
2019-09-02 13:43:08 +0800

    新建Vue项目
    Change-Id: Iaa372c15dfd2224716026baf94abfe56eb3cf304

---
+++ AboutMe/precache-manifest.aa710198305771c5332cc96208779440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "da5beaf91079a6ed792b",
+    "url": "js/chunk-vendors.9080b304.js"
+  },
+  {
+    "revision": "3ff8323998efbb5c57a9",
+    "url": "js/app.2a52cb94.js"
+  },
+  {
+    "revision": "1ed527f6f3d3dd26f569",
+    "url": "js/about.2e1661c7.js"
+  },
+  {
+    "revision": "e195e8ff225183ba2d193f2970e5fdab",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3ff8323998efbb5c57a9",
+    "url": "css/app.38f827ec.css"
+  }
+];
