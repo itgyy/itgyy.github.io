0eb595aa4fb8dec5b6c8aad0090c8069f2524baf
guyaoyao <851511476@qq.com>
2019-09-02 16:15:05 +0800

    no message
    Change-Id: Ie486a5952243fcb95a9baa7450c1f56be2aec7de

---
+++ dist/precache-manifest.71d24c475d9614626664bd8d95e85e57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "da5beaf91079a6ed792b",
+    "url": "/js/chunk-vendors.9080b304.js"
+  },
+  {
+    "revision": "d8ef0fa494c6e7538fc8",
+    "url": "/js/app.987348ea.js"
+  },
+  {
+    "revision": "9b2b6ada5d051209429b",
+    "url": "/js/about.8fd4cd74.js"
+  },
+  {
+    "revision": "10a86f197042dc7c9a76aa5bd9225c33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d8ef0fa494c6e7538fc8",
+    "url": "/css/app.38f827ec.css"
+  }
+];
